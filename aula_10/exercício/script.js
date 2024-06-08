d2c7cee7939656e22ba06afecc99dab8a0ebee50
madpais <madpais87@gmail.com>
2024-06-07 21:38:53 -0300

    up
    up

--- aula_10/exercício/script.js
+++ aula_10/exercício/script.js
@@ -1,5 +1,47 @@
-function change() {
-    const img = document.getElementById('btn_1');
-    const pacth = img.getAttribute("src")
+
+function change1() {
+    const img = document.getElementById('myImg1');
+    const currentScr = img.getAttribute("src");
+
+    if (currentScr === 'img/petter_1.jpg') {
+        img.setAttribute("src", "img/petter_2.jpg");
+        img.setAttribute("alt", "");
+
+    } else {
+        img.setAttribute("src", "img/petter_1.jpg");
+        img.setAttribute("alt", "");
+    }
+    
+}
+
+
+function change2() {
+    const img = document.getElementById('myImg2');
+    const currentScr = img.getAttribute("src");
+
+    if (currentScr === 'img/viúva_1.jpg') {
+        img.setAttribute("src", "img/viúva_2.jpg");
+        img.setAttribute("alt", "");
+
+    } else {
+        img.setAttribute("src", "img/viúva_1.jpg");
+        img.setAttribute("alt", "");
+    }
+    
+}
+
+
+function change3() {
+    const img = document.getElementById('myImg3');
+    const currentScr = img.getAttribute("src");
+
+    if (currentScr === 'img/mistica_1.png') {
+        img.setAttribute("src", "img/mistica_2.webp");
+        img.setAttribute("alt", "");
+
+    } else {
+        img.setAttribute("src", "img/mistica_1.png");
+        img.setAttribute("alt", "");
+    }
     
 }
