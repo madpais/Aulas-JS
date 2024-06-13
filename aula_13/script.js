04b942d0b9a4700e8d15d3863487d41d2d265845
madpais <madpais87@gmail.com>
2024-06-13 20:34:16 -0300

    aula13

---
+++ aula_13/script.js
@@ -0,0 +1,25 @@
+const input = document.getElementById("taskInput");
+
+const ul = document.getElementById("taskList");
+
+function addTask() {
+
+    if (input.value!== "") {
+        const li = document.createElement("li");
+        li.textContent = input.value;
+
+        const removerBotao = document.createElement("button");
+        removerBotao.textContent = "Remover";
+        removerBotao.onclick = function() {
+        ul.removeChild(li);
+        ul.appendChild(li);
+        li.appendChild(removerBotao);
+
+        input.value = "";
+
+        }
+
+    } else {
+        alert("Por favor insira uma tarefa")
+    }
+}
